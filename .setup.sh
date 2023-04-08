6da30350208dd7ba9e18c89001b26739d9afc4fe
killercho <ggeorgi60@gmail.com>
2023-04-08 23:13:25 +0300

    Added file for enabling tapping and scrolling.
    The file is created with the execution of setup.sh.

--- .setup.sh
+++ .setup.sh
@@ -87,6 +87,17 @@ then
                 echo "Background images should be located in /usr/share/backgrounds"
         fi
 
-    else
-        echo "Exiting..."
+        echo "Enabling tapping and natural scrolling..."
+        sudo mkdir -p /etc/X11/xorg.conf.d && sudo tee <<'EOM' /etc/X11/xorg.conf.d/90-touchpad.conf 1> /dev/null
+Section "InputClass"
+        Identifier "touchpad"
+        MatchIsTouchpad "on"
+        Driver "libinput"
+        Option "Tapping" "on"
+        Option "NaturalScrolling" "True"
+EndSection
+
+EOM
+else
+    echo "Exiting..."
 fi
